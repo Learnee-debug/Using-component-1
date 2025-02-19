a743cc8ab3dc9693ada6ecf7b55716f3895d81b8
Watcheer <shubhampadkonde12@gmail.com>
2025-02-19 09:03:02 +0530

    completed

--- src/components/bookcard.jsx
+++ src/components/bookcard.jsx
@@ -1,1 +1,22 @@
-// write the book component code here
+import PropTypes from "prop-types";
+import "./bookCard.css";
+
+const BookCard = ({ title, author, description, image }) => {
+  return (
+    <div className="book-card">
+      <img src={image} alt={title} className="book-image" />
+      <h2>{title}</h2>
+      <h4>by {author}</h4>
+      <p>{description}</p>
+    </div>
+  );
+};
+
+BookCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired, // Add image prop validation
+};
+
+export default BookCard;
